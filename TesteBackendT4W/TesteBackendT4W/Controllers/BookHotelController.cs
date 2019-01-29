f5a02c8766abb8b9b247dae937082d6fd2908dd5
Gabriel Estavaringo Ferreira <gabriel.estavaringo@gmail.com>
2019-01-29 10:59:05 -0200

    Update BookHotelController.cs

--- TesteBackendT4W/TesteBackendT4W/Controllers/BookHotelController.cs
+++ TesteBackendT4W/TesteBackendT4W/Controllers/BookHotelController.cs
@@ -13,7 +13,7 @@ namespace TesteBackendT4W.Controllers
     {
 
         private static readonly HttpClient _httpClient = new HttpClient();
-        private static readonly string address = "https://pp.cangooroo.net/ws/rest/hotel.svc/Search";
+        private static readonly string url = "https://pp.cangooroo.net/";
 
 
         // GET: BookHotel
@@ -42,23 +42,69 @@ namespace TesteBackendT4W.Controllers
             }
         }
 
-        // POST: BookHotel/Consult
+
+        // POST: BookHotel/Search
         [HttpPost]
-        public ActionResult Consult(BookHotelRequest request)
+        public ActionResult Search(BookHotelModelView bookHotelModelView)
         {
             try
             {
+            
+                //validate the model received by the form (if it's not validated in view)
+                if(!ModelState.IsValid){
+                
+                    return View("Index", bookHotelModelView);
+                    
+                }
+                
+                //create request
+                BookHotelRequest bookHotelRequest = new BookHotelRequest(bookHotelModelView);
+
+                //get response
+                BookHotelResponse bookHotelResponse =  await searchHotelRoomAsync(bookHotelRequest);
 
 
+                //orders by total selling price
+                bookHotelResponse.Hotels.Sort((x, y) => x.Rooms.TotalSellingPrice.Value.CompareTo(y.Rooms.TotalSellingPrice.Value));
+                
+                
+                //returns the list to be viewed
+                return View("Search", bookHotelResponse);
 
-                return View();
+                
             }
             catch
             {
                 return View("Error");
             }
         }
-
+        
+        [NonAction]
+        public static async BookHotelResponse searchHotelRoomAsync(BookHotelRequest request)
+        {
+            _htppClient.baseAddress = new Uri(url);
+            
+            // Add an Accept header for JSON format
+            _htppClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
+            
+            //send POST request
+            HttpResponseMessage response = await _htppClient.PostAsJsonAsync("ws/rest/hotel.svc/Search", request);
+            
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse and returns the response body.
+                return response.Content.ReadAsAsync<IEnumerable<bookHotelResponse>>().Result;  //Make sure to add a reference to System.Net.Http.Formatting.dll
+            }
+            
+            else
+            {
+                //request failed
+                return RedirectToAction("Index");
+            }
+        
+        }
+        
 
     }
 }
